0fab79e6cc9da6be627f31d3341c29e552eff655
Truong Anh Tuan <tuan188@gmail.com>
2016-01-20 09:40:51 +0700

    Add project

---
+++ MGMacProductDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MGMacProductDemo
+//
+//  Created by Tuan Truong on 1/20/16.
+//  Copyright © 2016 Tuan Truong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MGMacProductDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MGMacProductDemo
+//
+//  Created by Tuan Truong on 1/20/16.
+//  Copyright © 2016 Tuan Truong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
